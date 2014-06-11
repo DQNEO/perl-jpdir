84c8131b19f780f1da3bdd4412d6d904ee38b7b5
DQNEO <dqneoo@gmail.com>
2014-06-12 04:02:26 +0900

    refactor

--- cygwin.pl
+++ cygwin.pl
@@ -3,11 +3,5 @@ use strict;
 use warnings;
 use autodie;
 use Encode;
-
-# to utf8 binary
-rename 'a', 'しぐうぃん';
-
-{
-    use utf8;
-    rename 'b', Encode::encode('utf8', 'しぐうぃん２');
-}
+use utf8;
+rename 'cygwin', Encode::encode('utf8', 'しぐうぃん２');
